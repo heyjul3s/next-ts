3d9741c14c3cf4ae3edaf29c2fcbfdc7ee1dcbd7
heyjul3s <julian.cy.low@gmail.com>
2021-09-27 18:58:52 +1000

    add useGoogleMapsAPI hook

--- hooks/useGoogleMapsAPI.ts
+++ hooks/useGoogleMapsAPI.ts
@@ -1,35 +1,45 @@
 import React from 'react';
-import { Loader } from '@googlemaps/js-api-loader';
+import { Loader, LoaderOptions } from '@googlemaps/js-api-loader';
 
-export const Status = {
-  LOADING: 'LOADING',
-  FAILURE: 'FAILURE',
-  SUCCESS: 'SUCCESS'
+enum LoadStatus {
+  LOADING = 'LOADING',
+  FAILURE = 'FAILURE',
+  SUCCESS = 'SUCCESS'
+}
+
+type TLoadStatus = { [key in keyof LoadStatus]: LoadStatus[key] };
+
+export const loadingStatus = {
+  LOADING: LoadStatus.LOADING,
+  FAILURE: LoadStatus.FAILURE,
+  SUCCESS: LoadStatus.SUCCESS
 };
 
 export function useGoogleMapsAPI(options = { libraries: ['places'] }) {
-  const [status, setStatus] = React.useState(Status.LOADING);
+  const [status, setStatus] = React.useState<TLoadStatus>(
+    loadingStatus.LOADING
+  );
 
   React.useEffect(() => {
     const loader = new Loader({
       apiKey: process.env.GOOGLE_MAPS_API_KEY,
       ...options
-    });
+    } as LoaderOptions);
 
-    const setLoadStatus = (status) => {
+    const setLoadStatus = (status: TLoadStatus) => {
       setStatus(status);
     };
 
-    setLoadStatus(Status.LOADING);
+    setLoadStatus(loadingStatus.LOADING);
 
     loader.load().then(
-      () => setLoadStatus(Status.SUCCESS),
-      () => setLoadStatus(Status.FAILURE)
+      () => setLoadStatus(loadingStatus.SUCCESS),
+      () => setLoadStatus(loadingStatus.FAILURE)
     );
   }, []);
 
   return {
     status,
-    loading: status === Status.SUCCESS
+    loading: status === loadingStatus.SUCCESS
   };
 }
