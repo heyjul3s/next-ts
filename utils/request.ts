77f69d3cc9522e557a90d1e65f26eef1d29b074c
heyjul3s <julian.cy.low@gmail.com>
2021-10-14 22:07:53 +1100

    create request util

---
+++ utils/request.ts
@@ -0,0 +1,47 @@
+import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
+import qs from 'qs';
+import deepExtend from 'deep-extend';
+
+export class Request {
+  static DEFAULT_API_CONFIG = {
+    returnRejectedPromiseOnError: true,
+    timeout: 30000,
+    baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
+    headers: {
+      common: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      }
+    },
+    paramsSerializer: (params: string): string =>
+      qs.stringify(params, { indices: false })
+  };
+
+  public static get<Data extends unknown, Error extends unknown>(
+    endpoint: string,
+    config: AxiosRequestConfig = {}
+  ): () => Promise<Data> {
+    return Request.send<Data, Error>(endpoint, { ...config, method: 'GET' });
+  }
+
+  public static post<Data extends unknown, Error extends unknown>(
+    endpoint: string,
+    config: AxiosRequestConfig = {}
+  ): () => Promise<Data> {
+    return Request.send<Data, Error>(endpoint, { ...config, method: 'POST' });
+  }
+
+  public static send<Data extends unknown, Error extends unknown>(
+    endpoint: string,
+    config: AxiosRequestConfig = {}
+  ): () => Promise<Data> {
+    return async () => {
+      const response: AxiosResponse<Data> | AxiosError<Error> = await axios({
+        ...deepExtend(this.DEFAULT_API_CONFIG, config),
+        url: endpoint
+      });
+
+      return response.data;
+    };
+  }
+}
