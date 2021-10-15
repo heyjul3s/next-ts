a75f0745caca3945c6ebc94b9366ceea3bbaa609
heyjul3s <julian.cy.low@gmail.com>
2021-10-16 10:25:40 +1100

    export theme type

--- theme/index.ts
+++ theme/index.ts
@@ -2,6 +2,8 @@ import { extendTheme } from '@chakra-ui/react';
 import { breakpoints, colors, config, fonts, radii } from './foundations/index';
 import { components } from './components';
 
+export type AppTheme = typeof theme;
+
 export const theme = extendTheme({
   breakpoints,
   colors,
