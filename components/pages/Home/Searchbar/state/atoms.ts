2a7daf5301561009dc8378c0356fd7d1617d7ff8
heyjul3s <julian.cy.low@gmail.com>
2022-02-21 22:15:39 +1100

    remove redundant Searchbar component

--- components/pages/Home/Searchbar/state/atoms.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from 'recoil';
-
-export const searchbarQueryState = atom({
-  key: 'searchbarQueryState',
-  default: ''
-});
--- components/pages/Home/Searchbar/state/selectors.ts
+++
@@ -1,7 +0,0 @@
-import { selector } from 'recoil';
-import { searchbarQueryState } from './atoms';
-
-export const selectSearchbarQuery = selector({
-  key: 'selectSearchbarQuery',
-  get: ({ get }) => get(searchbarQueryState)
-});
