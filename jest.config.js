cfefe1d262335587c0055229453826b6b7337629
heyjul3s <julian.cy.low@gmail.com>
2022-02-21 22:03:18 +1100

    jest config update

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,30 @@
-module.exports = {
-  rootDir: '.',
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  // * Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './'
+});
+
+// * Add any custom config to be passed to Jest
+const customJestConfig = {
+  collectCoverageFrom: ['<rootDir>/src/js/**/*.{js,jsx,ts,tsx}'],
   collectCoverage: true,
   coverageThreshold: {
     global: {
-      branches: 90,
-      functions: 90,
-      lines: 90,
-      statements: 90
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50
     }
   },
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|scss)$': 'identity-obj-proxy'
-  },
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-  transform: {
-    '^.+\\.[jt]sx?$': 'babel-jest',
-    '^.+\\.(tsx|ts)?$': 'ts-jest'
-  },
+
+  // * Add more setup options before each test is run
+  // * setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  // * if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
   prettierPath: '<rootDir>/prettier.config.js'
 };
+
+// * createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
