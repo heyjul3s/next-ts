e875f4bf71f0e45a3c828936b49ded6bb33acfd8
heyjul3s <julian.cy.low@gmail.com>
2022-02-22 23:09:53 +1100

    add fuse.js fuzzy search for posts selector

--- state/posts/selectors.ts
+++ state/posts/selectors.ts
@@ -1,7 +1,45 @@
 import { selector } from 'recoil';
+import Fuse from 'fuse.js';
 import { postsState } from './atoms';
+import { searchbarQueryState } from '../searchBar/atoms';
+import { TPost } from '@/api/index';
+
+type TPostSearchResult = {
+  refIndex: number;
+  score?: number;
+  matches?: ReadonlyArray<Fuse.FuseResultMatch>;
+} & TPost;
 
 export const selectPosts = selector({
   key: 'selectPosts',
   get: ({ get }) => get(postsState)
+});
+
+export const selectPostsByQuery = selector({
+  key: 'selectPostsByQuery',
+  get: ({ get }) => {
+    const posts = get(postsState);
+    const searchQuery = get(searchbarQueryState);
+
+    const fuse = new Fuse(posts, {
+      includeScore: true,
+      keys: ['title']
+    });
+
+    const searchResults = fuse
+      .search(searchQuery)
+      .reduce((results, result: Fuse.FuseResult<TPost>) => {
+        return [
+          ...results,
+          {
+            ...result.item,
+            refIndex: result?.refIndex,
+            score: result?.score,
+            matches: result?.matches
+          }
+        ];
+      }, [] as TPostSearchResult[]);
+
+    return !searchQuery ? posts : searchResults;
+  }
 });
