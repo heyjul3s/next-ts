f7275502de7ee75e9503541707f8074e68a24e7c
heyjul3s <julian.cy.low@gmail.com>
2022-03-02 21:46:48 +1100

    update Form component

--- components/common/Form.tsx
+++ components/common/Form.tsx
@@ -1,22 +1,58 @@
-import { FormProvider, useForm } from 'react-hook-form';
+import { useState } from 'react';
+
+import {
+  FormProvider as ReactHookFormProvider,
+  useForm
+} from 'react-hook-form';
+
+import { joiResolver } from '@hookform/resolvers/joi';
 
 import type { ReactNode } from 'react';
-import type { UseFormProps } from 'react-hook-form';
+import type { FormProviderProps, UseFormProps } from 'react-hook-form';
 
-type TFormProps = {
+export interface IFormProps {
   children: ReactNode;
   config: UseFormProps;
-  onSubmitForm: any;
+  schema: any;
+  onSubmitForm: (values: any) => Promise<void | IOnSubmitResult>;
+}
+export interface IOnSubmitResult {
+  FORM_ERROR?: string;
+  [prop: string]: any;
+}
+
+type TFormProviderProps = FormProviderProps & {
+  submissionError?: string | null;
 };
 
-export function Form({ children, config, onSubmitForm }: TFormProps) {
+export const FORM_ERROR = 'FORM_ERROR';
+
+const FormProvider = ({ children, ...props }: TFormProviderProps) => (
+  <ReactHookFormProvider {...props}>{children}</ReactHookFormProvider>
+);
+
+export function Form({ children, config, onSubmitForm, schema }: IFormProps) {
   const form = useForm({
+    ...(!!config?.resolver && { resolver: joiResolver(schema) }),
     ...config
   });
 
+  const [formSubmissionError, setFormSubmissionError] = useState<string | null>(
+    null
+  );
+
   return (
-    <FormProvider {...form}>
-      <form noValidate onSubmit={form.handleSubmit(onSubmitForm)}>
+    <FormProvider {...form} submissionError={formSubmissionError}>
+      <form
+        noValidate
+        onSubmit={form.handleSubmit(async (values) => {
+          const result = (await onSubmitForm(values)) || {};
+
+          if (!!result.FORM_ERROR) {
+            setFormSubmissionError(result.FORM_ERROR);
+          }
+        })}
+      >
         {children}
       </form>
     </FormProvider>
