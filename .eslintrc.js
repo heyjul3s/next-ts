01cacf5eae35e552f0821a23dadad51c56439079
heyjul3s <julian.cy.low@gmail.com>
2022-03-02 22:22:45 +1100

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,13 @@
 module.exports = {
   ignorePatterns: ['node_modules/', '.next/', '.out/'],
-  env: {
-    es6: true,
-    browser: true,
-    jest: true,
-    node: true
-  },
-  settings: {
-    react: {
-      version: 'detect'
-    },
-    typescript: {}
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.json',
-    tsconfigRootDir: './'
-  },
-  plugins: ['@typescript-eslint', 'import'],
   extends: [
-    'plugin:@next/next/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:security/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
+    'next',
     'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
     'prettier'
   ],
   rules: {
-    'no-console': 1,
-    'react/prop-types': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    'prettier/prettier': 'error',
-    '@typescript-eslint/interface-name-prefix': 'off',
-    'react/react-in-jsx-scope': 'off'
+    'no-unused-vars': 'error'
   }
 };
