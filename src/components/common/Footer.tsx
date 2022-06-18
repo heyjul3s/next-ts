90c62d6ac219a08060fde22a9ccee031816a37ff
heyjul3s <julian.cy.low@gmail.com>
2022-06-18 16:31:18 +1000

    Remove Newsletter component

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { AnchorLink } from './AnchorLink';
-import { Box, Container, Flex, Spacer, Text } from '@chakra-ui/react';
-
-import { Newsletter } from './Newsletter';
+import { Box, Container, Spacer, Text } from '@chakra-ui/react';
 
 export function Footer() {
   const currentYear = new Date().getFullYear();
@@ -10,24 +8,18 @@ export function Footer() {
   return (
     <Box as="footer" p={6} background="gray.200">
       <Container maxW="container.xl">
-        <Flex py="4">
-          <Newsletter />
-        </Flex>
+        <Box py="2">
+          <Text size="xs">{`© Copyright ${currentYear} ${process.env.NEXT_PUBLIC_APP_NAME} All rights reserved`}</Text>
+        </Box>
 
-        <Flex py="4">
-          <Box py="2">
-            <Text size="xs">{`© Copyright ${currentYear} ${process.env.NEXT_PUBLIC_APP_NAME} All rights reserved`}</Text>
-          </Box>
+        <Spacer />
 
-          <Spacer />
-
-          <Box>
-            <AnchorLink href="/terms" mr="4">
-              Terms of use
-            </AnchorLink>
-            <AnchorLink href="/privacy-policy">Privacy Policy</AnchorLink>
-          </Box>
-        </Flex>
+        <Box>
+          <AnchorLink href="/terms" mr="4">
+            Terms of use
+          </AnchorLink>
+          <AnchorLink href="/privacy-policy">Privacy Policy</AnchorLink>
+        </Box>
       </Container>
     </Box>
   );
